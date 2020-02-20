8c346930691a9dd816cca3b73d57800797709ffc
Andrew <andrewdang52@gmail.com>
2020-02-20 12:36:21 -0800

    crap

--- src/DataStructures/Library.java
+++ src/DataStructures/Library.java
@@ -1,23 +1,27 @@
 package DataStructures;
 
 public class Library{
-        public int LibID;
-        public int books;
-        public int shippable;
-        public int signupTime;
+
+    private int LID;
+    private int numberBooks;
+    private int Shippable;
+    private int signup;
 
 
-        public void setLibID(int ID){
-            LibID = ID;
-        }
-        public void setBooks(int book){
-            books = book;
-        }
-        public void setShippable(int ship){
-            shippable = ship;
+    public Library(int LibID, int numB, int ship, int sign ){
+        this.LID =LibID;
+        this .numberBooks = numB;
+        this.Shippable = ship;
+        this.signup = sign;
+
     }
+    public int getLibID(){ return this.LID;}
 
+    public int getNumB(){return this.numberBooks;}
 
+    public int Shippable(){return this.Shippable;}
+
+    public 
 
 
 }
