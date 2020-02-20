4ef9df6742f0718a6d17cb9d92b8ef280b4efc0e
Andrew <andrewdang52@gmail.com>
2020-02-20 11:17:14 -0800

    Test

---
+++ Library.java
@@ -0,0 +1,22 @@
+
+
+public class Library{
+    class Library{
+        int LibID
+        int books;
+        int shippable;
+        int signupTime;
+
+
+        public int setLibID(int ID){
+            LibID = ID
+        }
+        public int setBooks(int book){
+            books = book
+        }
+    }
+
+
+}
+
+
