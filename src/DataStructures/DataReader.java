934911d36d07a19220f7a59e01b319f3575a8ead
Matthew Taing <taingmat@uw.edu>
2020-02-20 11:49:34 -0800

    Added main

--- src/DataStructures/DataReader.java
+++ src/DataStructures/DataReader.java
@@ -5,8 +5,8 @@ import java.io.*;
 public class DataReader {
     //Store the file to be read
 
-    public static void main(String[] args)throws Exception
-    {
-        File file = new File("a_example.txt")
+    public static void main(String[] args)throws Exception {
+        File file = new File("a_example.txt");
+        System.out.println("Hello World !!");
     }
 }
